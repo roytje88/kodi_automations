ebeb6fea9d27651e415e42c5dc6ce47ecb6f1d8f
roy <royvanrhedenburg@gmail.com>
2021-11-08 15:18:14 +0100

    added logging

--- autodelete.sh
+++ autodelete.sh
@@ -13,7 +13,17 @@ echo "Cleaning library..."
 curl --user kodi:kodi --data-binary '{ "jsonrpc": "2.0", "method": "VideoLibrary.Clean", "id": "mybash"}' -H 'content-type: application/json;' http://127.0.0.1:8080/jsonrpc
 
 
-python autodelete.py
+now=$(date +"%Y%m%d_%H:%I_autodelete.log")
+
+
+
+mkdir -p log
+python autodelete.py >> log/$now
+
+
+
+
+
 
 
 echo "Updating library one more time..."
